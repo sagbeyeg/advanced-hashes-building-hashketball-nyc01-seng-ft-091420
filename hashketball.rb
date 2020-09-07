089c3bbcb3d1800b23f47015ee2b36661c1cbadc
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-07 16:43:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -91,6 +91,16 @@ def game_hash
          :blocks => 5,
          :slam_dunks => 5},
          
+         {:player_name => "Ben Gorden",
+         :number => 8,
+         :shoe => 15,
+         :points => 33,
+         :rebounds => 3,
+         :assists => 2,
+         :steals => 1,
+         :blocks => 1,
+         :slam_dunks => 0},
+         
          {:player_name => "Alan Anderson",
          :number => 0,
          :shoe => 16,
