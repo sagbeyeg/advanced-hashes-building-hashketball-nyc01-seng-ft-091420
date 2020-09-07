8d50113561203a6c400d1c399859b88c53810df1
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-07 16:40:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -58,9 +58,19 @@ def game_hash
         ]
     },
     :away => {
-      :team_name => "",
-      :colors => [],
-      :players => ""
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [
+        {:player_name => "Jeff Adrien",
+         :number => 4,
+         :shoe => 18,
+         :points => 10,
+         :rebounds => 1,
+         :assists => 1,
+         :steals => 2,
+         :blocks => 1,
+         :slam_dunks => 1}
+        ]
     }
   }
 end
