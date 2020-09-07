b597276bf8dde588d5ba9cbbe715cdccd3f8c1cd
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-07 16:42:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -79,7 +79,17 @@ def game_hash
          :assists => 7,
          :steals => 22,
          :blocks => 15,
-         :slam_dunks => 10}
+         :slam_dunks => 10},
+         
+         {:player_name => "Desagna Diop",
+         :number => 2,
+         :shoe => 14,
+         :points => 24,
+         :rebounds => 12,
+         :assists => 12,
+         :steals => 4,
+         :blocks => 5,
+         :slam_dunks => 5}
         ]
     }
   }
